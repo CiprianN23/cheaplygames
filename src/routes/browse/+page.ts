8291e78302ecebb735eef0534ae23b1ec57c933e
CiprianN23 <neagoeciprian16@yahoo.com>
2022-09-09 15:51:50 +0300

    Added prettier

--- src/routes/browse/+page.ts
+++ src/routes/browse/+page.ts
@@ -1,19 +1,17 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, url }) => {
-    let storeID = url.searchParams.get('storeID');
-    let res;
+	let storeID = url.searchParams.get('storeID');
+	let res;
 
-    if (storeID === null)
-    {
-        res = await fetch('https://www.cheapshark.com/api/1.0/deals?pageSize=15');
-    }
-    else{
-        res = await fetch(`https://www.cheapshark.com/api/1.0/deals?pageSize=15&storeID=${storeID}`);
-    }
+	if (storeID === null) {
+		res = await fetch('https://www.cheapshark.com/api/1.0/deals?pageSize=15');
+	} else {
+		res = await fetch(`https://www.cheapshark.com/api/1.0/deals?pageSize=15&storeID=${storeID}`);
+	}
 
-    return {
-        deals: res.ok && await res.json(),
-        maxPages: res.headers.get('x-total-page-count')
-    }
-}
+	return {
+		deals: res.ok && (await res.json()),
+		maxPages: res.headers.get('x-total-page-count')
+	};
+};
