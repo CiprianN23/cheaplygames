4ff0462c36a7a83ec2886b9103b385cd7c04b0d7
CiprianN23 <neagoeciprian16@yahoo.com>
2022-09-15 17:30:31 +0300

    Change from google fonts to bunny.net for better privacy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,8 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.bunny.net" crossorigin="anonymous">
+	<link href="https://fonts.bunny.net/css?family=open-sans:400" rel="stylesheet" />
 </svelte:head>
 
 <Navbar />
