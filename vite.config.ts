43c6c6f8ee9afaff9c22d740ebbe3d5b5c28bcb0
CiprianN23 <neagoeciprian16@yahoo.com>
2023-07-24 14:23:02 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const stylifyPlugin = stylifyVite({
 		{ files: ['src/**/*.tsx'], outputFile: 'src/styles/global.css' }
 	],
   compiler: {
-      
+      mangleSelectors: process.env.STYLIFY_MANGLE_SELECTORS !== 'undefined',
       variables: {
         text: '#010304',
         background: '#eff6fb',
