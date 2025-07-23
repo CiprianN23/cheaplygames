5f060e0be67a0569fa5a7ec2d64af355369f158a
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2025-07-23 22:46:36 +0300

    Updated compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
-  compatibilityDate: "2025-03-13",
+  compatibilityDate: "2025-07-23",
   devtools: { enabled: true },
   modules: [
     "@nuxt/icon",
